c3fd7108a0bd2164d23d24345c2a8eb909d6fe14
Simão Martins <simao.martins@tecnico.ulisboa.pt>
2020-07-30 22:18:02 +0100

    Better handle of uri.

--- src/main/scala/pt/tecnico/dsi/openstack/nova/NovaClient.scala
+++ src/main/scala/pt/tecnico/dsi/openstack/nova/NovaClient.scala
@@ -1,16 +1,12 @@
 package pt.tecnico.dsi.openstack.nova
 
 import cats.effect.Sync
-import org.http4s.Uri.Path
 import org.http4s.client.Client
 import org.http4s.{Header, Uri}
 import pt.tecnico.dsi.openstack.nova.services.{Quotas, Servers}
 
 class NovaClient[F[_]: Sync](baseUri: Uri, authToken: Header)(implicit client: Client[F]) {
-	val uri: Uri = {
-		val lastSegment = baseUri.path.dropEndsWithSlash.segments.lastOption
-		if (lastSegment.contains(Path.Segment("v2.1"))) baseUri else baseUri / "v2.1"
-	}
+	val uri: Uri = if (baseUri.path.dropEndsWithSlash.toString.endsWith("v2.1")) baseUri else baseUri / "v2.1"
 
 	val quotas = new Quotas[F](uri, authToken)
 	val servers = new Servers[F](uri, authToken)
