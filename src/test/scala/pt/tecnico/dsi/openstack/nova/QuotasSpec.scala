3c98b337c150fce4183c6f7d0bc8d538762307bd
Simão Martins <simao.martins@tecnico.ulisboa.pt>
2020-07-30 22:16:11 +0100

    Better handling of quotas in tests.

--- src/test/scala/pt/tecnico/dsi/openstack/nova/QuotasSpec.scala
+++ src/test/scala/pt/tecnico/dsi/openstack/nova/QuotasSpec.scala
@@ -3,28 +3,63 @@ package pt.tecnico.dsi.openstack.nova
 import cats.effect.IO
 import org.http4s.client.UnexpectedStatus
 import org.scalatest.Assertion
-import pt.tecnico.dsi.openstack.nova.models.Quota
+import pt.tecnico.dsi.openstack.common.models.Usage
+import pt.tecnico.dsi.openstack.nova.models.{Quota, QuotaUsage}
 import squants.information.InformationConversions._
 
 class QuotasSpec extends Utils {
   import nova.quotas
 
+  val defaultQuotas = Quota(
+    cores = 20,
+    instances = 10,
+    keyPairs = 100,
+    metadataItems = 128,
+    ram = 50.gibibytes,
+    serverGroups = 10,
+    serverGroupMembers = 10,
+    fixedIps = -1,
+    floatingIps = 10,
+    securityGroups = 10,
+    securityGroupRules = 20,
+    injectedFiles = 5,
+    injectedFileContent = 10.kibibytes,
+    injectedFilePath = 255.bytes,
+  )
+
+  val defaultQuotaUsage = QuotaUsage(
+    cores = Usage(0, defaultQuotas.cores, 0),
+    instances = Usage(0, defaultQuotas.instances, 0),
+    keyPairs = Usage(0, defaultQuotas.keyPairs, 0),
+    metadataItems = Usage(0, defaultQuotas.metadataItems, 0),
+    ram = Usage(0.gibibytes, defaultQuotas.ram, 0.gibibytes),
+    serverGroups = Usage(0, defaultQuotas.serverGroups, 0),
+    serverGroupMembers = Usage(0, defaultQuotas.serverGroupMembers, 0),
+    fixedIps = Usage(0, defaultQuotas.fixedIps, 0),
+    floatingIps = Usage(0, defaultQuotas.floatingIps, 0),
+    securityGroups = Usage(0, defaultQuotas.securityGroups, 0),
+    securityGroupRules = Usage(0, defaultQuotas.securityGroupRules, 0),
+    injectedFiles = Usage(0, defaultQuotas.injectedFiles, 0),
+    injectedFileContent = Usage(0.gibibytes, defaultQuotas.injectedFileContent, 0.gibibytes),
+    injectedFilePath = Usage(0.gibibytes, defaultQuotas.injectedFilePath, 0.gibibytes),
+  )
+
   "Quotas service" should {
     "get quotas for a project (existing id)" in withStubProject.use[IO, Assertion] { project =>
-      quotas.get(project.id).idempotently(_.value.cores shouldBe 20)
+      quotas.get(project.id).idempotently(_.value shouldBe defaultQuotas)
     }
     "get quotas for a project (non-existing id)" in {
       quotas.get("non-existing-id").idempotently(_ shouldBe None)
     }
     "apply quotas for a project (existing id)" in withStubProject.use[IO, Assertion] { project =>
-      quotas.apply(project.id).idempotently(_.cores shouldBe 20)
+      quotas.apply(project.id).idempotently(_ shouldBe defaultQuotas)
     }
     "apply quotas for a project (non-existing id)" in {
       quotas.apply("non-existing-id").attempt.idempotently(_.left.value shouldBe a [UnexpectedStatus])
     }
 
     "get usage quotas for a project (existing id)" in withStubProject.use[IO, Assertion] { project =>
-      quotas.getUsage(project.id).idempotently(_.value.cores.limit shouldBe 20)
+      quotas.getUsage(project.id).idempotently(_.value shouldBe defaultQuotaUsage)
     }
     "get usage quotas for a project (non-existing id)" in {
       quotas.getUsage("non-existing-id").idempotently(_ shouldBe None)
@@ -35,23 +70,6 @@ class QuotasSpec extends Utils {
     "apply usage quotas for a project (non-existing id)" in {
       quotas.applyUsage("non-existing-id").attempt.idempotently(_.left.value shouldBe a [UnexpectedStatus])
     }
-
-    val defaultQuotas = Quota(
-      cores = 20,
-      instances = 10,
-      keyPairs = 100,
-      metadataItems = 128,
-      ram = 50.gibibytes,
-      serverGroups = 10,
-      serverGroupMembers = 10,
-      fixedIps = -1,
-      floatingIps = 10,
-      securityGroups = 10,
-      securityGroupRules = 20,
-      injectedFiles = 5,
-      injectedFileContent = 10.kibibytes,
-      injectedFilePath = 255.bytes,
-    )
 
     "get default quotas for a project (existing id)" in withStubProject.use[IO, Assertion] { project =>
       quotas.getDefaults(project.id).idempotently(_.value shouldBe defaultQuotas)
