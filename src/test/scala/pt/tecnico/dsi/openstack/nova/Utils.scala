ee28b0ac670024692d43c85fb8b523b8da584248
Simão Martins <simao.martins@tecnico.ulisboa.pt>
2021-01-05 12:53:47 +0000

    Update dependencies.

--- src/test/scala/pt/tecnico/dsi/openstack/nova/Utils.scala
+++ src/test/scala/pt/tecnico/dsi/openstack/nova/Utils.scala
@@ -32,8 +32,15 @@ abstract class Utils extends AsyncWordSpec with Matchers with BeforeAndAfterAll 
   
   override protected def afterAll(): Unit = finalizer.unsafeRunSync()
   
-  //import org.http4s.client.middleware.Logger
-  //implicit val httpClient: Client[IO] = Logger(logBody = true, logHeaders = true)(_httpClient)
+  /*import org.http4s.Headers
+    import org.http4s.client.middleware.{Logger, ResponseLogger}
+    import org.typelevel.ci.CIString
+    implicit val httpClient: Client[IO] = Logger.colored(
+      logHeaders = true,
+      logBody = true,
+      redactHeadersWhen = (Headers.SensitiveHeaders ++ List(CIString("X-Auth-Token"), CIString("X-Subject-Token"))).contains,
+      responseColor = ResponseLogger.defaultResponseColor[IO] _
+    )(_httpClient)*/
   implicit val httpClient: Client[IO] = _httpClient
   
   val keystone: KeystoneClient[IO] = KeystoneClient.fromEnvironment().unsafeRunSync()
