b453d46818a24aaad8c3f725554678a46afd91f5
Simão Martins <simao.martins@tecnico.ulisboa.pt>
2021-02-03 17:35:17 +0000

    Add getByName and applyByName to flavors.

--- src/main/scala/pt/tecnico/dsi/openstack/nova/services/Flavors.scala
+++ src/main/scala/pt/tecnico/dsi/openstack/nova/services/Flavors.scala
@@ -1,6 +1,8 @@
 package pt.tecnico.dsi.openstack.nova.services
 
 import cats.effect.Concurrent
+import cats.syntax.flatMap._
+import cats.syntax.functor._
 import fs2.Stream
 import io.circe.{Decoder, Encoder}
 import org.http4s.client.Client
@@ -11,6 +13,9 @@ import pt.tecnico.dsi.openstack.nova.models.Flavor
 
 // Flavors seem to be un-updatable. We are always receiving a "resource not found" no matter what
 
+/**
+ * @define domainModel Flavor
+ */
 final class Flavors[F[_]: Concurrent: Client](baseUri: Uri, session: Session)
   extends PartialCrudService[F](baseUri, "flavor", session.authToken)
     with CreateNonIdempotentOperations[F, Flavor, Flavor.Create]
@@ -20,6 +25,26 @@ final class Flavors[F[_]: Concurrent: Client](baseUri: Uri, session: Session)
   
   override implicit val createEncoder: Encoder[Flavor.Create] = Flavor.Create.encoder
   override implicit val modelDecoder: Decoder[Flavor] = Flavor.decoder
+  
+  /**
+   * Gets the $domainModel with the specified `name`.
+   * @param name the name of the $domainModel to get.
+   * @note Nova does not allow listing/searching flavors by name. So this method lists all flavors and finds the one named `name`.
+   * @return a Some with the $domainModel if it exists. If more than one exists the first one. A None otherwise.
+   */
+  def getByName(name: String): F[Option[Flavor]] = list().map(_.find(_.name == name))
+  
+  /**
+   * Gets the $domainModel with the specified `name`, assuming it exists.
+   * @param name the name of the $domainModel to get.
+   * @note Nova does not allow listing/searching flavors by name. So this method lists all flavors and finds the one named `name`.
+   * @return the $domainModel with the given `name`. If more than one exists the first one. If none exists F will contain an error.
+   */
+  def applyByName(name: String): F[Flavor] =
+    getByName(name).flatMap {
+      case Some(model) => F.pure(model)
+      case None => F.raiseError(new NoSuchElementException(s"""Could not find ${this.name} with name "$name"."""))
+    }
   
   /**
    * Lists summary information for all flavors.
